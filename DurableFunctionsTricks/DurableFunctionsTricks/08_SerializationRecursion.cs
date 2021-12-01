c272ae972e54eaf97addfeb59706f27d3fcdb967
LBugnion <lbugnion@outlook.com>
2021-12-01 16:21:03 +0100

    Fixing sample

--- DurableFunctionsTricks/DurableFunctionsTricks/08_SerializationRecursion.cs
+++ DurableFunctionsTricks/DurableFunctionsTricks/08_SerializationRecursion.cs
@@ -27,7 +27,10 @@ namespace DurableFunctionsTricks
 
             series.Modules = new List<LearnLiveModule>
             {
-                new LearnLiveModule(series, "First module")
+                new LearnLiveModule("First module")
+                {
+                    Parent = series
+                }
             };
 
             outputs.Add(await context.CallActivityAsync<string>(
--- DurableFunctionsTricks/DurableFunctionsTricks/Model/LearnLiveModule.cs
+++ DurableFunctionsTricks/DurableFunctionsTricks/Model/LearnLiveModule.cs
@@ -4,19 +4,12 @@ namespace DurableFunctionsTricks.Model
 {
     public class LearnLiveModule
     {
-        [JsonIgnore]
         public LearnLiveSeries Parent { get; set; }
 
         public string Title { get; set; }
 
         public LearnLiveModule(string title)
         {
-            Title = title;
-        }
-
-        public LearnLiveModule(LearnLiveSeries parent, string title)
-        {
-            Parent = parent;
             Title = title;
         }
     }
